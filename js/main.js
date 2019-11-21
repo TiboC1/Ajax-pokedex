9a6941f47374e526dd2d33b8cb37d116e405dca1
Tibo Colman <tibocolman@gmail.com>
2019-11-21 14:43:55 +0100

    search function and styling

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ Promise.all(promises).then(results => {
     id: data.id,
     image: data.sprites["front_default"],
     type: data.types.map(type => type.type.name).join(", "),
+    abilities: data.abilities.map(ability => ability.ability.name). join(", ")
   }));
 
   const displayPokemon = pokemon => {
@@ -21,12 +22,29 @@ Promise.all(promises).then(results => {
           <img class="card-image" src="${pokeman.image}"/>
           <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
           <p class="card-subtitle">Type: ${pokeman.type}</p>
+          <p class="card-subtitle">Abilities: ${pokeman.abilities}</p>
       </li>
       `
       )
       .join("");
+
     pokedex.innerHTML = pokemonHTMLString;
   };
   displayPokemon(pokemon);
+
 });
 
+function search_pokemon() { 
+  let input = document.getElementById('searchbar').value.toLowerCase()
+  let x = document.getElementsByClassName('card');
+  let title = document.getElementsByClassName('card-title');
+    
+  for (i = 0; i < x.length; i++) {  
+      if (!title[i].innerHTML.toLowerCase().includes(input)) { 
+          x[i].style.display="none"; 
+
+      } else { 
+          x[i].style.display="list-item";  
+      } 
+  }
+}
